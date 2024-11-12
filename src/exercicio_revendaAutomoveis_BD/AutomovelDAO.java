a27e6a10222c0bccb06f7ec0566eb7cae09a867e
carolini <bassan804@gmail.com>
2024-11-11 21:02:22 -0300

    debug conflitos

--- src/exercicio_revendaAutomoveis_BD/AutomovelDAO.java
+++ src/exercicio_revendaAutomoveis_BD/AutomovelDAO.java
@@ -7,22 +7,23 @@ import java.sql.SQLException;
 import java.util.LinkedList;
 
 public class AutomovelDAO {
-
+	
 	private Connection conexao;
 
 	public AutomovelDAO() {
 		this.conexao = Conexao.getConexao();
 	}
-
-	public LinkedList<String> listarMarcas() {
+	
+	public LinkedList<String> listarMarcas(){
 		String sql = "SELECT marca FROM automovel";
 		LinkedList<String> marcas = new LinkedList<String>();
 		try {
-			PreparedStatement ps = (PreparedStatement) conexao.prepareStatement(sql);
-			ResultSet rs = ps.executeQuery();
-			while (rs.next()) {
+			PreparedStatement ps = (PreparedStatement) 
+					conexao.prepareStatement(sql);
+			ResultSet rs = ps.executeQuery();			
+			while(rs.next()) {				
 				marcas.add(rs.getString("marca"));
-			}
+			}		
 			ps.close();
 			rs.close();
 			return marcas;
@@ -30,72 +31,76 @@ public class AutomovelDAO {
 			System.out.println("Erro ao consultar automoveis");
 			return null;
 		}
-
+		
 	}
-
+	
 	public boolean inserir(Automovel A) {
-
-		String sql = "INSERT INTO automovel (marca, modelo, ano, cor, combustivel) VALUES (?, ?, ?, ?, ?)";
-
+		
+		String sql = "INSERT INTO automovel (marca, modelo, ano, cor, combustivel) VALUES (?, ?, ?, ?, ?)";		
+		
 		try {
-			PreparedStatement ps = (PreparedStatement) conexao.prepareStatement(sql);
+			PreparedStatement ps = (PreparedStatement)
+					conexao.prepareStatement(sql);
 			ps.setString(1, A.getMarca());
 			ps.setString(2, A.getModelo());
 			ps.setString(3, A.getAno());
 			ps.setString(4, A.getCor());
 			ps.setString(5, A.getCombustivel());
-			boolean retorno = ps.execute();
+			boolean retorno = ps.execute();				
 			ps.close();
 			return retorno;
 		} catch (SQLException e) {
 			System.out.println("Erro ao Inserir automoveis");
 			return false;
 		}
-
+		
 	}
-
-	public Automovel consultarMarca(String marca) {
-		String sql = "SELECT * FROM automovel WHERE marca = ?";
+	
+	
+	public Automovel consultarMarca(String marca){
+		String sql = "SELECT * FROM automovel WHERE marca = ?";		
 		try {
-			PreparedStatement ps = (PreparedStatement) conexao.prepareStatement(sql);
+			PreparedStatement ps = (PreparedStatement) 
+					conexao.prepareStatement(sql);
 			ps.setString(1, marca);
 			ResultSet rs = ps.executeQuery();
-
-			if (rs.next()) {
-				Automovel A = new Automovel(rs.getString("marca"), rs.getString("modelo"), rs.getString("ano"),
-						rs.getString("cor"), rs.getString("combustivel"));
+			
+			if(rs.next()) {
+				Automovel A = new Automovel(rs.getString("marca"),rs.getString("modelo"),rs.getString("ano"), rs.getString("cor"), rs.getString("combustivel"));
 				A.setId(rs.getInt("id"));
 				return A;
-			}
+			}		
 			ps.close();
 			return null;
 		} catch (SQLException e) {
 			System.out.println("Erro ao consultar automoveis");
 			return null;
 		}
-
+		
 	}
-
-	public LinkedList<Automovel> listar() {
+	
+	
+	public LinkedList<Automovel> listar(){
 		String sql = "SELECT * FROM automovel";
 		LinkedList<Automovel> lista = new LinkedList<Automovel>();
 		try {
-			PreparedStatement ps = (PreparedStatement) conexao.prepareStatement(sql);
+			PreparedStatement ps = (PreparedStatement) 
+					conexao.prepareStatement(sql);
 			ResultSet rs = ps.executeQuery();
-
-			while (rs.next()) {
-				Automovel A = new Automovel(rs.getString("marca"), rs.getString("modelo"), rs.getString("ano"),
-						rs.getString("cor"), rs.getString("combustivel"));
+			
+			while(rs.next()) {
+				Automovel A =new Automovel(rs.getString("marca"),rs.getString("modelo"),rs.getString("ano"), rs.getString("cor"), rs.getString("combustivel"));
 				A.setId(rs.getInt("id"));
 				lista.add(A);
-			}
+			}		
 			ps.close();
 			return lista;
 		} catch (SQLException e) {
 			System.out.println("Erro ao consultar Automoveis");
 			return null;
 		}
-
+		
 	}
-
+	
+	
 }
--- src/exercicio_revendaAutomoveis_BD/TelaCadastro.java
+++ src/exercicio_revendaAutomoveis_BD/TelaCadastro.java
@@ -11,168 +11,101 @@ import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import net.miginfocom.swing.MigLayout;
-import java.awt.Font;
-import java.awt.SystemColor;
-import java.awt.Color;
 
 public class TelaCadastro extends JFrame {
 
-    private static final long serialVersionUID = 1L;
-    private JPanel contentPane;
-    private JLabel lblNewLabel;
-    private JLabel lblNewLabel_1;
-    private JLabel lblNewLabel_2;
-    private JLabel lblNewLabel_3;
-    private JTextField marca;
-    private JTextField modelo;
-    private JTextField ano;
-    private JButton botao;
-    private final JTextField cor = new JTextField();
-    private final JTextField comb = new JTextField();
-    private final JLabel lblNewLabel_3_1 = new JLabel("Cor:");
-    private final JLabel lblNewLabel_3_1_1 = new JLabel("Combustivel");
+        private static final long serialVersionUID = 1L;
+        private JPanel contentPane;
+        private JLabel lblNewLabel;
+        private JLabel lblNewLabel_1;
+        private JLabel lblNewLabel_2;
+        private JLabel lblNewLabel_3;
+        private JTextField marca;
+        private JTextField modelo;
+        private JTextField ano;
+        private JButton botao;
+        private final JTextField cor = new JTextField();
+        private final JTextField comb = new JTextField();
+        private final JLabel lblNewLabel_3_1 = new JLabel("Cor:");
+        private final JLabel lblNewLabel_3_1_1 = new JLabel("Combustivel");
+        private JLabel lblNewLabel_4;
 
-    public static void main(String[] args) {
-        EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                try {
-                    TelaCadastro frame = new TelaCadastro();
-                    frame.setVisible(true);
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
-        });
-    }
-
-    private void acaoBotao() {
-        Automovel f = new Automovel(marca.getText(), modelo.getText(), ano.getText(), cor.getText(), comb.getText());
-        AutomovelDAO dao = new AutomovelDAO();
-        if (!dao.inserir(f)) {
-            JOptionPane.showMessageDialog(null, "Inserido");
+        public static void main(String[] args) {
+                EventQueue.invokeLater(new Runnable() {
+                        public void run() {
+                                try {
+                                        TelaCadastro frame = new TelaCadastro();
+                                        frame.setVisible(true);
+                                } catch (Exception e) {
+                                        e.printStackTrace();
+                                }
+                        }
+                });
         }
-    }
 
-    public TelaCadastro() {
-        this.comb.setColumns(10);
-        this.cor.setColumns(10);
-        initComponents();
-    }
+        private void acaoBotao() {
+                Automovel f = new Automovel(marca.getText(), modelo.getText(), ano.getText(), cor.getText(),
+                                comb.getText());
+                AutomovelDAO dao = new AutomovelDAO();
+                if (!dao.inserir(f)) {
+                        JOptionPane.showMessageDialog(null, "Inserido");
+                }
+        }
 
-    private void initComponents() {
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setBounds(100, 100, 587, 430);
-        this.contentPane = new JPanel();
-        this.contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+        public TelaCadastro() {
+                this.comb.setColumns(10);
+                this.cor.setColumns(10);
+                initComponents();
+        }
 
-        setContentPane(this.contentPane);
+        private void initComponents() {
+                setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                setBounds(100, 100, 587, 430);
+                this.contentPane = new JPanel();
+                this.contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
-        this.lblNewLabel = new JLabel("<html><h1>Inserir Automóvel</h1></html>");
+                setContentPane(this.contentPane);
 
-        this.lblNewLabel_1 = new JLabel("Marca:");
-        this.lblNewLabel_2 = new JLabel("Modelo:");
-        this.lblNewLabel_3 = new JLabel("Ano:");
+                this.lblNewLabel = new JLabel("<html><h1>Inserir Automóvel</h1></html>");
 
-        this.marca = new JTextField();
-        this.marca.setColumns(10);
+                this.lblNewLabel_1 = new JLabel("Marca:");
+                this.lblNewLabel_2 = new JLabel("Modelo:");
+                this.lblNewLabel_3 = new JLabel("Ano:");
 
-        this.modelo = new JTextField();
-        this.modelo.setColumns(10);
+                this.marca = new JTextField();
+                this.marca.setColumns(10);
 
-        this.ano = new JTextField();
-        this.ano.setColumns(10);
+                this.modelo = new JTextField();
+                this.modelo.setColumns(10);
 
-        this.botao = new JButton("Inserir");
-        this.botao.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                acaoBotao();
-            }
-        });
-        GroupLayout gl_contentPane = new GroupLayout(this.contentPane);
-        gl_contentPane.setHorizontalGroup(
-                gl_contentPane.createParallelGroup(Alignment.LEADING)
-                        .addGroup(Alignment.TRAILING, gl_contentPane.createSequentialGroup()
-                                .addContainerGap(336, Short.MAX_VALUE)
-                                .addComponent(botao)
-                                .addGap(330))
-                        .addGroup(gl_contentPane.createSequentialGroup()
-                                .addGap(129)
-                                .addGroup(gl_contentPane.createParallelGroup(Alignment.TRAILING)
-                                        .addGroup(gl_contentPane.createSequentialGroup()
-                                                .addComponent(this.lblNewLabel_3_1_1, GroupLayout.PREFERRED_SIZE, 79,
-                                                        GroupLayout.PREFERRED_SIZE)
-                                                .addPreferredGap(ComponentPlacement.RELATED)
-                                                .addComponent(this.comb, GroupLayout.PREFERRED_SIZE, 334,
-                                                        GroupLayout.PREFERRED_SIZE))
-                                        .addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
-                                                .addGroup(gl_contentPane.createSequentialGroup()
-                                                        .addGap(118)
-                                                        .addComponent(lblNewLabel))
-                                                .addGroup(gl_contentPane.createSequentialGroup()
-                                                        .addGroup(gl_contentPane.createParallelGroup(Alignment.TRAILING)
-                                                                .addComponent(this.lblNewLabel_3_1,
-                                                                        GroupLayout.PREFERRED_SIZE, 79,
-                                                                        GroupLayout.PREFERRED_SIZE)
-                                                                .addGroup(gl_contentPane
-                                                                        .createParallelGroup(Alignment.LEADING, false)
-                                                                        .addComponent(lblNewLabel_1, Alignment.TRAILING,
-                                                                                GroupLayout.DEFAULT_SIZE,
-                                                                                GroupLayout.DEFAULT_SIZE,
-                                                                                Short.MAX_VALUE)
-                                                                        .addComponent(lblNewLabel_2, Alignment.TRAILING,
-                                                                                GroupLayout.DEFAULT_SIZE,
-                                                                                GroupLayout.DEFAULT_SIZE,
-                                                                                Short.MAX_VALUE)
-                                                                        .addComponent(lblNewLabel_3, Alignment.TRAILING,
-                                                                                GroupLayout.PREFERRED_SIZE, 79,
-                                                                                GroupLayout.PREFERRED_SIZE)))
-                                                        .addPreferredGap(ComponentPlacement.RELATED)
-                                                        .addGroup(gl_contentPane.createParallelGroup(Alignment.TRAILING)
-                                                                .addComponent(ano, GroupLayout.PREFERRED_SIZE, 334,
-                                                                        GroupLayout.PREFERRED_SIZE)
-                                                                .addGroup(gl_contentPane
-                                                                        .createParallelGroup(Alignment.LEADING)
-                                                                        .addComponent(marca, GroupLayout.PREFERRED_SIZE,
-                                                                                334, GroupLayout.PREFERRED_SIZE)
-                                                                        .addComponent(modelo,
-                                                                                GroupLayout.PREFERRED_SIZE, 334,
-                                                                                GroupLayout.PREFERRED_SIZE))
-                                                                .addComponent(this.cor, GroupLayout.PREFERRED_SIZE, 334,
-                                                                        GroupLayout.PREFERRED_SIZE)))))
-                                .addContainerGap(183, Short.MAX_VALUE)));
-        gl_contentPane.setVerticalGroup(
-                gl_contentPane.createParallelGroup(Alignment.LEADING)
-                        .addGroup(gl_contentPane.createSequentialGroup()
-                                .addGap(45)
-                                .addComponent(lblNewLabel)
-                                .addGap(63)
-                                .addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
-                                        .addComponent(lblNewLabel_1)
-                                        .addComponent(marca, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE,
-                                                GroupLayout.PREFERRED_SIZE))
-                                .addPreferredGap(ComponentPlacement.RELATED)
-                                .addGroup(gl_contentPane.createParallelGroup(Alignment.TRAILING)
-                                        .addComponent(modelo, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE,
-                                                GroupLayout.PREFERRED_SIZE)
-                                        .addComponent(lblNewLabel_2))
-                                .addPreferredGap(ComponentPlacement.RELATED)
-                                .addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
-                                        .addComponent(lblNewLabel_3)
-                                        .addComponent(ano, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE,
-                                                GroupLayout.PREFERRED_SIZE))
-                                .addPreferredGap(ComponentPlacement.RELATED)
-                                .addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
-                                        .addComponent(this.cor, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE,
-                                                GroupLayout.PREFERRED_SIZE)
-                                        .addComponent(this.lblNewLabel_3_1))
-                                .addPreferredGap(ComponentPlacement.RELATED)
-                                .addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
-                                        .addComponent(this.comb, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE,
-                                                GroupLayout.PREFERRED_SIZE)
-                                        .addComponent(this.lblNewLabel_3_1_1))
-                                .addPreferredGap(ComponentPlacement.RELATED, 33, Short.MAX_VALUE)
-                                .addComponent(botao)
-                                .addGap(53)));
-        this.contentPane.setLayout(gl_contentPane);
-    }
+                this.ano = new JTextField();
+                this.ano.setColumns(10);
+
+                this.botao = new JButton("Inserir");
+                this.botao.addActionListener(new ActionListener() {
+                        public void actionPerformed(ActionEvent e) {
+                                acaoBotao();
+                        }
+                });
+
+                contentPane.setLayout(new MigLayout("", "[grow][85px][200.00][41.00][grow]",
+                                "[grow][49px][20px][20px][20px][20px][20px][23px][grow]"));
+
+                contentPane.add(lblNewLabel, "cell 1 1 2 1,alignx center,growy");
+                contentPane.add(lblNewLabel_1, "cell 1 2,alignx right,aligny center");
+                contentPane.add(lblNewLabel_2, "cell 1 3,alignx right,aligny center");
+                contentPane.add(lblNewLabel_3, "cell 1 5,alignx right,aligny center");
+                contentPane.add(lblNewLabel_3_1_1, "cell 1 4,alignx right,aligny bottom");
+
+                contentPane.add(marca, "cell 2 2,growx,aligny top");
+                contentPane.add(modelo, "cell 2 3,growx,aligny top");
+                contentPane.add(ano, "flowx,cell 2 5,growx,aligny top");
+                
+                lblNewLabel_4 = new JLabel("Cor:");
+                contentPane.add(lblNewLabel_4, "cell 2 5");
+                contentPane.add(cor, "cell 2 5,growx,aligny top");
+                contentPane.add(comb, "cell 2 4,growx,aligny top");
+
+                contentPane.add(botao, "cell 2 7,alignx center,aligny top");
+        }
 }
