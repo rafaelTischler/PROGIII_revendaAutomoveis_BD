696ba8e8cf462c0858413d30217b40ffc2691263
Carolini Bassan <bassan804@gmail.com>
2024-11-30 21:09:37 -0300

    funcionalidades adicionas a pagina de menu principal

--- src/exercicio_revendaAutomoveis_BD/JPanel_menuPrincipal.java
+++ src/exercicio_revendaAutomoveis_BD/JPanel_menuPrincipal.java
@@ -3,8 +3,12 @@ package exercicio_revendaAutomoveis_BD;
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.Graphics;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
 import javax.swing.ImageIcon;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import net.miginfocom.swing.MigLayout;
 
@@ -33,14 +37,10 @@ public class JPanel_menuPrincipal extends JPanel {
     }
 
     private void initComponents() {
-        // Configuração do JPanel principal
         setBackground(Color.GRAY);
         setBounds(100, 100, 1280, 720);
-
-        // MigLayout Principal: remove espaçamento entre os painéis, mas mantém os internos
         setLayout(new MigLayout("insets 0, gap 0", "[grow][grow][grow]", "[25px][50px][grow]"));
 
-        // Configuração do panel_logo
         panel_logo.setBackground(new Color(198, 68, 50));
         panel_logo.setLayout(new MigLayout("insets 10, gap 10", "[grow][][grow]", "[grow][][grow]"));
         this.lbl_logoPanel.setForeground(Color.WHITE);
@@ -48,7 +48,6 @@ public class JPanel_menuPrincipal extends JPanel {
         panel_logo.add(lbl_logoPanel, "cell 1 1");
         add(panel_logo, "cell 0 0 1 2,grow");
 
-        // Configuração do panel_usuario
         panel_usuario.setBackground(Color.DARK_GRAY);
         panel_usuario.setLayout(new MigLayout("insets 10, gap 10", "[][grow]", "[]"));
         lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 11));
@@ -56,7 +55,6 @@ public class JPanel_menuPrincipal extends JPanel {
         panel_usuario.add(lblNewLabel, "cell 0 0");
         add(panel_usuario, "cell 1 0 2 1,grow");
 
-        // Configuração do panel_menu
         panel_menu.setBackground(Color.WHITE);
         panel_menu.setLayout(new MigLayout("insets 10, gap 10", "[grow][][grow][][grow][][grow][][grow]", "[grow][][grow]"));
         lblNewLabel_1.setFont(new Font("Tahoma", Font.BOLD, 12));
@@ -68,5 +66,50 @@ public class JPanel_menuPrincipal extends JPanel {
         lblNewLabel_2_1_1.setFont(new Font("Tahoma", Font.PLAIN, 12));
         panel_menu.add(lblNewLabel_2_1_1, "cell 7 1");
         add(panel_menu, "cell 1 1 2 1,grow");
+        
+        addMouseListenerToLabel(lblNewLabel_2, this::abrirCadastroAutomoveis);
+        addMouseListenerToLabel(lblNewLabel_2_1, this::abrirConsultaAutomoveis);
+        addMouseListenerToLabel(lblNewLabel_2_1_1, this::abrirAlterarAutomoveis);
+    }
+    
+    private void addMouseListenerToLabel(JLabel label, Runnable action) {
+        label.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                action.run();
+            }
+
+            @Override
+            public void mouseEntered(MouseEvent e) {
+                label.setForeground(Color.BLUE); 
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+                label.setForeground(Color.BLACK);
+            }
+        });
+    }
+
+ 
+    private void abrirCadastroAutomoveis() {
+        JPanel_cadastroAutomoveis cadastroPanel = new JPanel_cadastroAutomoveis();
+        atualizarTela(cadastroPanel);
+    }
+
+    private void abrirConsultaAutomoveis() {
+        JOptionPane.showMessageDialog(this, "Funcionalidade de consulta não implementada.");
+    }
+
+    private void abrirAlterarAutomoveis() {
+        JPanel_alterarAutomoveis alterarPanel = new JPanel_alterarAutomoveis();
+        atualizarTela(alterarPanel);
+    }
+
+    private void atualizarTela(JPanel novoPainel) {
+        removeAll();
+        add(novoPainel, "grow");
+        revalidate();
+        repaint();
     }
 }
